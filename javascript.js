074d77d902c950bdcc53546a9c699a431a1c0a92
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-09-26 10:56:30 +0200

    Add function firstNumber operator and SecondNumber

--- javascript.js
+++ javascript.js
@@ -2,3 +2,7 @@ add = (number1, number2) => number1 + number2;
 subtract = (number1, number2) => number1 - number2;
 multiply = (number1, number2) => number1 * number2;
 devide = (number1, number2) => number1 / number2;
+
+firstNumber = (number1) => number1;
+operator = (operator) => operator;
+secondNumber = (number2) => number2;
