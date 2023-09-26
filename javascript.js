5a3625ab31c22ed577276771267fea460c15eda0
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-09-26 13:35:21 +0200

    Add function operate

--- javascript.js
+++ javascript.js
@@ -6,3 +6,15 @@ devide = (number1, number2) => number1 / number2;
 firstNumber = (number1) => number1;
 operator = (operator) => operator;
 secondNumber = (number2) => number2;
+
+operate = ((number1, operator, number2) => {
+    if (operator === "+"){
+        return add(number1, number2);
+    } else if (operator === "-"){
+        return subtract(number1, number2)
+    } else if (operator === "*"){
+        return multiply(number1, number2)
+    } else {
+        return devide(number1, number2)
+    }
+})
