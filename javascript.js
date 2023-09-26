75d3aec99c799a6883d3fabbbb4b684aedc69e40
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-09-26 10:47:13 +0200

    Add functions add, subtract, multiply, devide

--- javascript.js
+++ javascript.js
@@ -1,1 +1,4 @@
-
+add = (number1, number2) => number1 + number2;
+subtract = (number1, number2) => number1 - number2;
+multiply = (number1, number2) => number1 * number2;
+devide = (number1, number2) => number1 / number2;
